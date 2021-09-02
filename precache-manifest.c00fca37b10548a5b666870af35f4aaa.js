63904210350751b9c7afefe7c9925a2757de1da9
defina <defina.inc@yandex.ru>
2021-09-02 15:06:26 +0300

    new app

---
+++ precache-manifest.c00fca37b10548a5b666870af35f4aaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "796cf3f22a68033932965b9b684b4bd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "952298886d67865c5e5e",
+    "url": "/static/css/main.5d9f047c.chunk.css"
+  },
+  {
+    "revision": "2647fc020d533f3ba2f6",
+    "url": "/static/js/2.739f23ad.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.739f23ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "952298886d67865c5e5e",
+    "url": "/static/js/main.e72b1f4e.chunk.js"
+  },
+  {
+    "revision": "00dc2562c30d8f74faeb",
+    "url": "/static/js/runtime-main.f311bd62.js"
+  }
+]);
